c1a9f8090401d55956748e90da2485dbbc7f0eda
Tireur2cables <nicolas.battaglini@hotmail.fr>
2020-12-13 20:02:18 +0100

    Correction de l'erreur de compilation onlinejudge
    Problem : 
    
    Solution : 
    
    Note :

--- nicolasBattaglini/problemF.c
+++ nicolasBattaglini/problemF.c
@@ -14,14 +14,15 @@ int main(int argc, char const *argv[]) {
 	int n = atoi(buf);
 
 	int res = 0;
-	for (int i = 0; i < n; i++) {
+	int i;
+	for (i = 0; i < n; i++) {
 		buf = NULL;
 		getline(&buf, &num, stdin);
 		int wi = atoi(strtok(buf, " "));
 		int li = atoi(strtok(NULL, " "));
 		res += (wi * li);
 	}
-	
+
 	free(buf);
 
 	int length = res / width;
